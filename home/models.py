44b436068b39c8249982769a07bb567725544123
Juha Yrjölä <juha.yrjola@iki.fi>
2015-10-07 22:28:42 +0300

    Tune homepage

--- home/models.py
+++ home/models.py
@@ -1,14 +1,30 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils.functional import cached_property
 
 from wagtail.wagtailcore.models import Page
 from wagtail.wagtailcore.fields import RichTextField
 from wagtail.wagtailadmin.edit_handlers import FieldPanel
 
+from apimanager.models import APIPage
+from projects.models import ProjectIndexPage
+from blog.models import BlogIndexPage
+
 
 class HomePage(Page):
     body = RichTextField(blank=True)
+
+    def api_pages(self):
+        return APIPage.objects.live()
+
+    @cached_property
+    def blog_index(self):
+        return BlogIndexPage.objects.live().first()
+
+    @cached_property
+    def project_index(self):
+        return ProjectIndexPage.objects.live().first()
 
     content_panels = Page.content_panels + [
         FieldPanel('body', classname="full")
---
+++ home/templatetags/home_tags.py
@@ -0,0 +1,12 @@
+from django import template
+
+
+register = template.Library()
+
+
+@register.filter
+def first_p(value):
+    paras = value.split('</p>')
+    if len(paras):
+        return paras[0] + '</p>'
+    return ''
