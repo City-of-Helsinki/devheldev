b952d54fe0e567a9a45329ae78342f712728f33d
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2015-10-16 16:49:42 +0300

    Added Kong configuration checker and some docs

--- apimanager/manager.py
+++ apimanager/manager.py
@@ -4,17 +4,46 @@ API Management interface to Kong
 """
 
 from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
 
 import kong.client
 
 
+def _kong_client():
+    """
+    If Kong URL is in settings, retuns a APIAdminClient instance configured
+    there
+
+    :return:kong.client.APIAdminClient
+    """
+    if settings.KONG_URL:
+        return kong.client.APIAdminClient('http://' + settings.KONG_URL)
+    else:
+        raise ImproperlyConfigured('Kong installation URL missing')
+
+
 def get_api_count():
-    kcli = kong.client.APIAdminClient('http://' + settings.KONG_URL)
+    """
+    Returns amount of APIs in configured in Kong
+
+    :return:int
+    """
+
+    kcli = _kong_client()
     return kcli.count()
 
 
 def create_api(name, upstream_url, request_host):
-    kcli = kong.client.APIAdminClient('http://' + settings.KONG_URL)
+    """
+    Create an API gateway
+
+    :param name:short name for the API
+    :param upstream_url:location of the API as an full URL
+    :param request_host:host and domain API server is recognized by Kong (HTTP Host)
+    :return:
+    """
+
+    kcli = _kong_client()
     result = kcli.create(upstream_url=upstream_url, name=name, request_host=request_host)
     if result['id']:
         return True
@@ -23,6 +52,13 @@ def create_api(name, upstream_url, request_host):
 
 
 def remove_api(name):
-    kcli = kong.client.APIAdminClient('http://' + settings.KONG_URL)
+    """
+    Remove API gateway
+
+    :param name:short name of the API
+    :return:None
+    """
+
+    kcli = _kong_client()
     kcli.delete(name)
 
