542f69a474130e2e957bd6fe0f942f3da0dba909
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2015-10-22 18:30:27 +0300

    API management platform's models and migrations
    These models are for Kong API configuration and user management

--- apimanager/models.py
+++ apimanager/models.py
@@ -8,6 +8,8 @@ from wagtail.wagtailsearch import index
 
 class APIPage(Orderable, Page):
     name = models.CharField(max_length=300, blank=False, null=False)
+    api_path = models.CharField("Root path for API Management platform", max_length=50, default="",
+                                help_text="User visible path; example usage: api.hel.fi/{path}/")
     location = models.URLField(blank=False, null=False)
     documentation = models.URLField(blank=True)
     short_description = models.TextField(blank=False)
@@ -34,3 +36,41 @@ class APIIndexPage(Page):
 
     def apis(self):
         return APIPage.objects.live()
+
+
+class KongAPIConfiguration(models.Model):
+    api_page = models.ForeignKey(APIPage)
+    request_host = models.CharField('Hostname for API management platform', max_length="150",
+                                    default="api.hel.fi",
+                                    help_text="Similar to HTTP virtual hosts; changing this from default is required if user visible API host is publicly different")
+    kong_api_id = models.CharField(max_length=300, editable=False)
+
+
+from django.conf import settings
+class APICustomer(models.Model):
+    """
+    Link between user account and API customer
+    """
+    account = models.ForeignKey(settings.AUTH_USER_MODEL)
+    enabled = models.BooleanField(default=False)
+
+
+class Application(models.Model):
+    """
+    Individual application for the API customer
+    containing one Kong consumer and its API key
+    """
+    customer = models.ForeignKey(APICustomer)
+    subscriptions = models.ManyToManyField(KongAPIConfiguration, through='APISubscription')
+    name = models.CharField(max_length=300)
+    description = models.TextField(blank=True, null=True)
+    location = models.URLField(blank=True, null=True)
+
+
+class APISubscription(models.Model):
+    api = models.ForeignKey(KongAPIConfiguration)
+    application = models.ForeignKey(Application)
+    consumer_id = models.CharField(max_length=300)
+    consumer_kong_id = models.CharField(max_length=300)
+    key = models.CharField(max_length=300)
+    key_kong_id = models.CharField(max_length=300)
