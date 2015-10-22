6898585531d894dd389ab97b5ad4ee1d8ef219be
Rikuoja <riku.oja@kapsi.fi>
2015-10-22 23:43:03 +0300

    Prettier commit watch

--- github/models.py
+++ github/models.py
@@ -1,5 +1,6 @@
 import json, requests
 from wagtail.wagtailcore.models import Page, Orderable
+import django.utils.dateparse as dateparse
 
 class GithubIndexPage(Page):
 
@@ -7,4 +8,6 @@ class GithubIndexPage(Page):
         url = "https://api.github.com/orgs/City-of-Helsinki/events?per_page=5"
         response = requests.get(url)
         events = response.json()
+        for index, event in enumerate(events):
+            events[index]['created_at'] = dateparse.parse_datetime(event['created_at'])
         return events
