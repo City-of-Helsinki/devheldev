161ce67e143d0417bcd8f6a13a0470f442e9e8a3
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2015-10-26 17:14:01 +0200

    Added APIs to subscribe to, some layout tweaks

--- apimanager/views.py
+++ apimanager/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
 import wtforms
 
+from .models import APIPage
+
 def add_application(request):
 
-    class ApplicationForm(wtforms.Form):
-        name = wtforms.StringField('nimi', [wtforms.validators.Length(min=5, max=300),
-                                            wtforms.validators.InputRequired()])
-        description = wtforms.TextAreaField('kuvaus', [wtforms.validators.optional()])
+    class APISubscriptionForm(wtforms.Form):
+        api = wtforms.SelectField("Subscribe to an API", default=0,
+            choices=[(0, '---')], coerce=int)
 
-    return render(request, 'apimanager/api_formi.html', {'form':ApplicationForm()})
+    class ApplicationForm(wtforms.Form):
+        name = wtforms.StringField('name', [wtforms.validators.Length(min=5, max=300),
+                                            wtforms.validators.InputRequired()])
+        description = wtforms.TextAreaField('description', [wtforms.validators.optional()])
+        location = wtforms.StringField('homepage location', [wtforms.validators.URL(),
+                                                    wtforms.validators.optional()])
+
+        subscriptions = wtforms.FieldList(wtforms.FormField(APISubscriptionForm), min_entries=2)
+
+    form = ApplicationForm()
+
+    # All entries fields share their choices apparently
+    form.subscriptions.entries[0].api.choices.extend([(api.pk, api.name) for api in APIPage.objects.all()])
+
+    return render(request, 'apimanager/api_formi.html', {'form': form})
