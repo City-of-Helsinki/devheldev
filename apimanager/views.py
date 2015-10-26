8fefa6e82a88fc0254fdcb5aab2492ad6f4b1b1c
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2015-10-26 17:42:55 +0200

    Added unsubscribe buttons, subscription fields now have always one extra field

--- apimanager/views.py
+++ apimanager/views.py
@@ -16,11 +16,21 @@ def add_application(request):
         location = wtforms.StringField('homepage location', [wtforms.validators.URL(),
                                                     wtforms.validators.optional()])
 
-        subscriptions = wtforms.FieldList(wtforms.FormField(APISubscriptionForm), min_entries=2)
+        subscribe = wtforms.FieldList(wtforms.FormField(APISubscriptionForm), min_entries=1)
 
-    form = ApplicationForm()
+    if request.POST:
+        form = ApplicationForm(request.POST)
+        add_extra(form)
+    else:
+        form = ApplicationForm()
 
     # All entries fields share their choices apparently
-    form.subscriptions.entries[0].api.choices.extend([(api.pk, api.name) for api in APIPage.objects.all()])
+    form.subscribe.entries[0].api.choices.extend([(api.pk, api.name) for api in APIPage.objects.all()])
+    subs = ((1, 'Vanha respa'), (2, 'Toinen API'))
 
-    return render(request, 'apimanager/api_formi.html', {'form': form})
+
+    return render(request, 'apimanager/api_formi.html', {'form': form, 'subscriptions' : subs})
+
+def add_extra(form):
+    if form.subscribe.entries[-1].api.data:
+        form.subscribe.append_entry()
