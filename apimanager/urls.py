40e2bffac71162ce7b223b577219766865206055
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2015-10-27 15:44:28 +0200

    Successful registration redirects to apps page

--- apimanager/urls.py
+++ apimanager/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import include, url
 
 urlpatterns = (
     url(r'add/$', 'apimanager.views.add_application', name='add_application'),
+    url(r'apps/$', 'apimanager.views.view_applications', name='view_applications')
 )
--- apimanager/views.py
+++ apimanager/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 import django.forms as forms
 
-from .models import APIPage
+from .models import APIPage, Application
 
 def get_api_choices():
     return [(api.pk, api.name) for api in APIPage.objects.all()]
@@ -13,6 +15,10 @@ class ApplicationForm(forms.Form):
 
     subscribe = forms.MultipleChoiceField(label="Subscribe to an API", choices=get_api_choices,
                                           widget=forms.CheckboxSelectMultiple, required=False)
+def register(data):
+    print('registering')
+
+
 
 def add_application(request):
 
@@ -21,6 +27,7 @@ def add_application(request):
         app_form = ApplicationForm(request.POST)
         if request.POST.get('save_ok') and app_form.is_valid():
             register(app_form.cleaned_data)
+            return HttpResponseRedirect(reverse('apimanager:view_applications'))
         elif request.POST.get('save') and app_form.is_valid():
             ask_save = True
     else:
@@ -34,5 +41,11 @@ def add_application(request):
                   'apimanager/api_formi.html',
                   {'form': app_form, 'subscriptions' : subs, 'ask_save': ask_save})
 
-def register(data):
-    print('registering')
+
+def view_applications(request):
+
+    apps = Application.objects.all()
+
+    return render(request,
+                  'apimanager/api_app_list.html',
+                  {'apps': apps})
