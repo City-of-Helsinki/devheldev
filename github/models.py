03d1133ba636d83cb573994f48c27b2b04ddbe56
Rikuoja <riku.oja@kapsi.fi>
2015-10-28 19:33:09 +0200

    Add simple github cache

--- github/models.py
+++ github/models.py
@@ -3,6 +3,7 @@ from wagtail.wagtailadmin.edit_handlers import FieldPanel
 from wagtail.wagtailcore.models import Page, Orderable
 import django.utils.dateparse as dateparse
 from django.db import models
+from django.core.cache import cache
 
 
 class GithubOrgIndexPage(Page):
@@ -13,8 +14,11 @@ class GithubOrgIndexPage(Page):
      ]
 
     def events(self):
-        response = requests.get('https://api.github.com/orgs/' + self.github_org_name + '/events?per_page=5')
-        events = response.json()
+        events = cache.get('events')
+        if not events:
+            cache.add('events',
+                               requests.get('https://api.github.com/orgs/' + self.github_org_name + '/events?per_page=5').json(), 60)
+            events = cache.get('events')
         for index, event in enumerate(events):
             events[index]['created_at'] = dateparse.parse_datetime(event['created_at'])
         return events
