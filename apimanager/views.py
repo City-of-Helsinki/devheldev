efd890c1f942a9c0c227aa8217a50bc3560f43ec
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2015-10-29 23:32:17 +0200

    Registering Applications and subscribing to APIs works
    Also, listing user's apps and subs has some content now

--- apimanager/views.py
+++ apimanager/views.py
@@ -3,20 +3,38 @@ from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 import django.forms as forms
 
-from .models import APIPage, Application
+from .models import APIPage, Application, APISubscription
 
 def get_api_choices():
     return [(api.pk, api.name) for api in APIPage.objects.all()]
 
 class ApplicationForm(forms.Form):
-    name = forms.CharField(label='name')
-    description = forms.CharField(label='description', widget=forms.Textarea)
+    name = forms.CharField(label='name', required=True)
+    description = forms.CharField(label='description', widget=forms.Textarea,
+                                  required=True)
     location = forms.URLField(label='homepage location', required=False)
 
-    subscribe = forms.MultipleChoiceField(label="Subscribe to an API", choices=get_api_choices,
-                                          widget=forms.CheckboxSelectMultiple, required=False)
-def register(data):
-    print('registering')
+    subscribe = forms.MultipleChoiceField(label="Subscribe to an API",
+                                          choices=get_api_choices,
+                                          widget=forms.CheckboxSelectMultiple,
+                                          required=False)
+
+def register(data, user):
+
+    app = Application.objects.create(
+        user=user,
+        name=data['name'],
+        description=data['description'],
+        location=data['location']
+    )
+
+    for sub in data['subscribe']:
+        api = APIPage.objects.get(pk=sub)
+        apisub = APISubscription(
+            api=api.kongapiconfiguration,
+            application=app
+        )
+        apisub.save()
 
 def add_application(request):
 
@@ -24,7 +42,7 @@ def add_application(request):
     if request.POST:
         app_form = ApplicationForm(request.POST)
         if request.POST.get('save_ok') and app_form.is_valid():
-            register(app_form.cleaned_data)
+            register(app_form.cleaned_data, request.user)
             return HttpResponseRedirect(reverse('apimanager:view_applications'))
         elif request.POST.get('save') and app_form.is_valid():
             ask_save = True
