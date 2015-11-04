b296092005c438eda7ca084c8e571e09d35dae1b
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2015-11-04 16:45:18 +0200

    Superfluous debug statements away

--- apimanager/apps.py
+++ apimanager/apps.py
@@ -8,7 +8,4 @@ class APIManagerConfig(AppConfig):
     verbose_name = "for managing API gateway Kong"
 
     def ready(self):
-        print("importing")
         import apimanager.signals
-        print("imported", apimanager.signals.handle_subscription_save)
-
