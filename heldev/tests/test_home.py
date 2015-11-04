81ad73b6fed171c5548bcab38180cdc93cc2ba96
Juha Yrjölä <juha.yrjola@iki.fi>
2015-11-04 22:13:19 +0200

    Make sure the test creates an event index page

--- heldev/tests/test_home.py
+++ heldev/tests/test_home.py
@@ -2,7 +2,7 @@ import pytest
 
 
 @pytest.mark.django_db
-def test_home_page(client):
+def test_home_page(client, index_pages):
     response = client.get('/')
     assert response.status_code == 200
     assert 'Helsinki Region Infoshare' in str(response.content)
