e1397e2eed2ec6ec48752438986035d1b21d1c14
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2015-11-04 15:55:33 +0200

    Subscription deletion signal attached

--- apimanager/signals.py
+++ apimanager/signals.py
@@ -1,10 +1,15 @@
 
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
 from .models import APISubscription
+
 
 @receiver(post_save, sender=APISubscription)
 def handle_subscription_save(sender, **kwargs):
     print("Sub saved!")
 
+
+@receiver(post_delete, sender=APISubscription)
+def handle_subscription_delete(sender, **kwargs):
+    print("Sub deleted!")
