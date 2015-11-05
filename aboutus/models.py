dfa47e1531c5a3a02d9037c8ec18b6603bcdd7e0
Juha Yrjölä <juha.yrjola@iki.fi>
2015-11-05 17:36:16 +0200

    Link User and PersonPage models

--- aboutus/models.py
+++ aboutus/models.py
@@ -1,14 +1,15 @@
 import requests
 from django.db import models
+from django.conf import settings
 
 from wagtail.wagtailcore.models import Page, Orderable
 from wagtail.wagtailsearch import index
 from wagtail.wagtailadmin.edit_handlers import FieldPanel
 
-# Create your models here.
-
 
 class PersonPage(Orderable, Page):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, null=True, blank=True,
+                                related_name='person', on_delete=models.SET_NULL)
     name = models.CharField(max_length=200, null=False)
     contact = models.URLField(null=False)
     github_user = models.CharField(max_length=200, blank=True, null=True)
@@ -28,10 +29,16 @@ class PersonPage(Orderable, Page):
         FieldPanel('name'),
         FieldPanel('contact'),
         FieldPanel('github_user'),
+        FieldPanel('user'),
         FieldPanel('job_title'),
         FieldPanel('description'),
         FieldPanel('listed')
     ]
+
+    def get_github_link(self):
+        if not self.github_user:
+            return None
+        return 'https://github.com/%s' % self.github_user
 
     def save(self, *args, **kwargs):
         # update the avatar url
