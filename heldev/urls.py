ff53aceac0d9a48f636a96a403e7d70e47896f72
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2015-11-24 19:06:56 +0200

    PEP 8 fix

--- heldev/urls.py
+++ heldev/urls.py
@@ -40,8 +40,6 @@ urlpatterns = [
     url(r'', include(wagtail_urls)),
 ]
 
-
-
 if settings.DEBUG:
     from django.conf.urls.static import static
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
