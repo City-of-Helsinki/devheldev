d940001ce3715c7777c3a5e83ad3072e18d6af87
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2019-07-02 14:33:43 +0300

    Fix github events rendering event messages

--- fetch_updates.py
+++ fetch_updates.py
@@ -1,6 +1,5 @@
-
-
 import requests
+
 
 def tmpl_hri_item_list(items):
     return "\n".join(
@@ -16,29 +15,34 @@ def tmpl_hri_item_list(items):
         ]
     )
 
+
 def get_hri_items(limit=5):
     try:
         response = requests.get(
             url="https://hri.fi/data/api/action/current_package_list_with_resources",
-            params={
-                "limit": limit,
-            },
+            params={"limit": limit},
         )
-        print('Response HTTP Status Code: {status_code}'.format(
-            status_code=response.status_code))
-        print('Response HTTP Response Body: {content}'.format(
-            content=response.content))
+        print(
+            "Response HTTP Status Code: {status_code}".format(
+                status_code=response.status_code
+            )
+        )
+        print("Response HTTP Response Body: {content}".format(content=response.content))
     except requests.exceptions.RequestException:
-        print('HTTP Request failed')
+        print("HTTP Request failed")
 
     data = response.json()
     if data.get("success"):
         return data.get("result")
 
+
 def do_hri_items():
     data = get_hri_items()
 
-    items = [(d["title_translated"]["en"] or d["title_translated"]["fi"] , d["name"]) for d in data]
+    items = [
+        (d["title_translated"]["en"] or d["title_translated"]["fi"], d["name"])
+        for d in data
+    ]
 
     resp = tmpl_hri_item_list(items)
 
@@ -46,26 +50,30 @@ def do_hri_items():
 
 
 def fetch_github_events(organisation="City-of-Helsinki", amount=40):
-    
-    response = requests.get(f"https://api.github.com/orgs/{organisation}/events?per_page={amount}")
-    
+
+    response = requests.get(
+        f"https://api.github.com/orgs/{organisation}/events?per_page={amount}"
+    )
+
     if response.status_code == 200:
-    
+
         events = response.json()
         for index, event in enumerate(events):
-            event['repo']['url'] = event['repo']['url'].replace('https://api.github.com/repos/', 'https://github.com/')
-    
+            event["repo"]["url"] = event["repo"]["url"].replace(
+                "https://api.github.com/repos/", "https://github.com/"
+            )
+
     return events[:amount]
 
 
 def github_event_tmpl(event, event_url, event_description):
-    
-    return f"""<li class='commit-list__item' />
-    <div class="commit-list__avatar"><img src="{event['actor']['avatar_url']}&s=128" alt="{event['actor']['login']}"></div>
+
+    return f"""<li class='commit-l==t__item' />
+    <div class="commit-l==t__avatar"><img src="{event['actor']['avatar_url']}&s=128" alt="{event['actor']['login']}"></div>
     <a href="{event_url}">
-    <div class="commit-list__date"><time datetime="{event['created_at']}"></time></div>
-    <div class="commit-list__description">
-    <span class="commit-list__actor">{event['actor']['login']}</span> {event_description} <span class="commit-list__repo">{event['repo']['name']}</span>
+    <div class="commit-l==t__date">{event['created_at']}"</div>
+    <div class="commit-l==t__description">
+    <span class="commit-l==t__actor">{event['actor']['login']}</span> {event_description} <span class="commit-l==t__repo">{event['repo']['name']}</span>
     </div>
     </a>
 </div></li>
@@ -75,44 +83,50 @@ def github_event_tmpl(event, event_url, event_description):
 def do_github_events(events):
 
     out = []
-    
+
     for event in events:
-    
-        if event['type'] is "PushEvent":
-            event_url = event['repo']['url']
-            branch = event['payload']['ref'].replace("refs/heads/", "")
+
+        if event["type"] == "PushEvent":
+            event_url = event["repo"]["url"]
+            branch = event["payload"]["ref"].replace("refs/heads/", "")
             event_description = f" pushed to {branch} at "
-        elif event['type'] is "IssueCommentEvent":
-            event_url = event['payload']['issue']['html_url']
-            event_description = f" {event['payload']['action']} comment on issue #{event['payload']['issue']['number']} at "
-        elif event['type'] is "IssuesEvent":
-            event_url = event['payload']['issue']['html_url']
-            event_description = f" {event['payload']['action']} issue #{event['payload']['issue']['number']} at "
-        elif event['type'] is "PullRequestEvent":
-            event_url = event['payload']['pull_request']['html_url']
+        elif event["type"] == "==sueCommentEvent":
+            event_url = event["payload"]["==sue"]["html_url"]
+            event_description = f" {event['payload']['action']} comment on ==sue #{event['payload']['==sue']['number']} at "
+        elif event["type"] == "==suesEvent":
+            event_url = event["payload"]["==sue"]["html_url"]
+            event_description = f" {event['payload']['action']} ==sue #{event['payload']['==sue']['number']} at "
+        elif event["type"] == "PullRequestEvent":
+            event_url = event["payload"]["pull_request"]["html_url"]
             event_description = f" {event['payload']['action']} pull request #{event['payload']['pull_request']['number']} at "
-        elif event['type'] is "MemberEvent":
-            event_url = event['payload']['member']['html_url']
+        elif event["type"] == "MemberEvent":
+            event_url = event["payload"]["member"]["html_url"]
             event_description = f" {event['payload']['action']} {event['payload']['member']['login']} to "
-        elif event['type'] is "ForkEvent":
-            event_url = event['payload']['forkee']['html_url']
-            event_description = f" created fork {event['payload']['forkee']['full_name']} from "
-        elif event['type'] is "WatchEvent":
-            event_url = event['repo']['url']
+        elif event["type"] == "ForkEvent":
+            event_url = event["payload"]["forkee"]["html_url"]
+            event_description = (
+                f" created fork {event['payload']['forkee']['full_name']} from "
+            )
+        elif event["type"] == "WatchEvent":
+            event_url = event["repo"]["url"]
             event_description = f" {event['payload']['action']} watching "
-        elif event['type'] is "PullRequestReviewCommentEvent":
-            event_url = event['payload']['comment']['html_url']
+        elif event["type"] == "PullRequestReviewCommentEvent":
+            event_url = event["payload"]["comment"]["html_url"]
             event_description = f" {event['payload']['action']} comment on pull request #{event['payload']['pull_request']['number']} at "
-        elif event['type'] is "CreateEvent":
-            event_url = event['repo']['url']
-            event_description = f" created {event['payload']['ref_type']} {event['payload']['ref']} at "
-        elif event['type'] is "DeleteEvent":
-            event_url = event['repo']['url']
-            event_description = f" deleted {event['payload']['ref_type']} {event['payload']['ref']} at "
+        elif event["type"] == "CreateEvent":
+            event_url = event["repo"]["url"]
+            event_description = (
+                f" created {event['payload']['ref_type']} {event['payload']['ref']} at "
+            )
+        elif event["type"] == "DeleteEvent":
+            event_url = event["repo"]["url"]
+            event_description = (
+                f" deleted {event['payload']['ref_type']} {event['payload']['ref']} at "
+            )
         else:
-            event_url = event['repo']['url']
+            event_url = event["repo"]["url"]
             event_description = f" - {event['type']} - "
 
         out.append(github_event_tmpl(event, event_url, event_description))
 
-    return ''.join(out)
+    return "".join(out)
